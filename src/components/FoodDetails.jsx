095c4928d71fb43da5d8c1f7a6cf997ffedb874c
ashrith-77 <ashrithr.15@gmail.com>
2025-05-15 22:12:46 +0530

    almost completed but minor changes are required

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-
+import styles from './fooddetails.module.css'
+import ItemList from './ItemList'
 const FoodDetails = ({ foodId }) => {
 
     const [food, setFood] = useState({})
-    const [isLoading,setIsLoading]=useState(true)
+    const [isLoading, setIsLoading] = useState(true)
     const URL = `https://api.spoonacular.com/recipes/${foodId}/information`
     const apiKey = "a042c645b66e42399868b661551fab66"
 
@@ -19,34 +20,38 @@ const FoodDetails = ({ foodId }) => {
     }, [foodId, URL])
     return (
         <div>
-            <div>
-                <h1>{food.title}</h1>
-                <img src={food.image} alt="" />
-                <div>
-                <span>
-                    <strong>⏱️{food.readyInMinutes} Minutes</strong>
-                </span>
-                <span>
-                    👨‍👩‍👧‍👦<strong>Serves:{food.servings}</strong>
-                </span>
-                <span>
-                    {food.vegetarian ? "🥕Vegetarian" : "🍖Non-Vegetarian"}
-                </span>
+            <div className={styles.recipeCard}>
+                <h1 className={styles.recipeName}>{food.title}</h1>
+                <img className={styles.recipeImage} src={food.image} alt="" />
+                <div className={styles.recipeDetails}>
+                    <span>
+                        <strong>⏱️{food.readyInMinutes} Minutes</strong>
+                    </span>
+                    <span>
+                        👨‍👩‍👧‍👦<strong>Serves:{food.servings}</strong>
+                    </span>
+                    <span>
+                        <strong>{food.vegetarian ? "🥕Vegetarian" : "🍖Non-Vegetarian"}</strong>
+                    </span>
                 </div>
                 <div>
-                    $<span>{(food.pricePerServing/100).toFixed(2)}</span>
+                    $<span><strong>{(food.pricePerServing / 100).toFixed(2)}</strong></span>
                 </div>
-            </div>
-            <div>
+                <h2>Ingredients</h2>
+                <ItemList food={food} isLoading={isLoading}/>
                 <h2>Instructions</h2>
-                {isLoading?(
-                    <p>Loading...</p>
-                ):(
-                    food.analyzedInstructions[0].steps.map((step,index)=>(<li key={index}>{step.step}</li>))
-                )}
+                <div className={styles.recipeInstructions}>
+                    <ol>
+                        {isLoading ? (
+                            <p>Loading...</p>
+                        ) : (
+                            food.analyzedInstructions[0].steps.map((step, index) => (<li key={index}>{step.step}</li>))
+                        )}
+                    </ol>
+                </div>
             </div>
         </div>
-    )
+            )
 }
 
-export default FoodDetails
+            export default FoodDetails
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Item from './item'
+
+const ItemList = ({ food, isLoading }) => {
+    return (
+        <div>
+            {isLoading ?
+                (<p>Loading...</p>) : (food.extendedIngredients.map((item) => <Item item={item}/>))}
+        </div>
+    )
+}
+
+export default ItemList
